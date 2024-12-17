4dafe9639469b4bd26b5fbf4f1fbf5693a7d9768
Alex Yosef <stokesbga@gmail.com>
2024-12-17 11:42:07 -0500

    aws testupload script

--- tests/aws_testupload.py
+++ tests/aws_testupload.py
@@ -2,21 +2,20 @@ import boto3
 import json
 
 
-# post_data = {
-#     "files": [{"file": "audio_file", open("app/demo.wav", "rb"))]
-# }
+post_data = {
+    "files": [("audio_file", open("app/demo.wav", "rb"))]
+}
 
 # Endpoint name
 endpoint_name = 'eg-en-translator-endpoint'
 
 # Invoke SageMaker endpoint
 client = boto3.client('sagemaker-runtime', region_name='us-east-1')
-print('client created')
 response = client.invoke_endpoint(
     EndpointName=endpoint_name,
     ContentType='application/json',
-    # Body=json.dumps(post_data)  # serialize your input data
-    Body="fileb://$app/demo.wav"
+    Body=json.dumps(post_data)
+    # Body="fileb://$app/demo.wav"
 )
 
 # Deserialize the response
